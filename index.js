4a79addbf1142dc65184a6f61f480a61908cfbe2
Fernando Lobo Martins <fernando.martins@serpro.gov.br>
2019-11-25 13:46:19 -0300

    colocando servidor na porta do heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ http.createServer(function(request, response) {
    
    // Manda o corpo da resposta "Olá Mundo"
    response.end('Olá Mundo\n');
-}).listen(443);
+}).listen(process.env.PORT);
 
 // Imprime no console a URL de acesso ao servidor
 console.log('Servidor executando em http://127.0.0.1:443/');
